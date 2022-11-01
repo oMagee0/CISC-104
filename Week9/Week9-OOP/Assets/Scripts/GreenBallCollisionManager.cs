3c7c8260d93e9bc4efce1e951aa62745852d101e
oMagee0 <omagee@udel.edu>
2022-11-01 17:55:57 -0400

    Week 9 Homework

---
+++ Week9/Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+
+    public AudioSource myClip;
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        // Homework: Do something interesting here
+        //Should play the Yippee sound when green ball collides with red ball
+        if(OtherBall.GetComponent<RedBallCollisionManager>() != null)
+        {
+            myClip.Play();
+            Debug.Log("Sound should play");
+        }
+        
+    }
+}
+
---
+++ Week9/Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        // Homework: Do something interesting here
+        //Not doing anything here since functionality is already implemented in GreenBallCollisionManager
+    }
+}
+
